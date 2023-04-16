b84942d368f3950faee91a2d44373802e916aa79
shin0343 <shin0343@mail.hongik.ac.kr>
2023-04-16 10:08:17 +0900

    기본 웹페이지 생성을 위한 HTML, Controller 작성

---
+++ demo/src/main/java/timmyroom/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package timmyroom.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello") // HTTP Get 방식
+    public String hello(Model model) {
+        model.addAttribute("data", "Timmyroom");
+        return "hello";
+    }
+}
