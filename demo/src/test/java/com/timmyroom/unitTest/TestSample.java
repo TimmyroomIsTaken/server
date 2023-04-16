f09a57b5675626f012eef58e9df2f58399336f69
choi_unhak <76737193+big-uh-show@users.noreply.github.com>
2023-04-16 16:55:49 +0900

    TC 성공 PR

--- demo/src/test/java/com/timmyroom/unitTest/TestSample.java
+++ demo/src/test/java/com/timmyroom/unitTest/TestSample.java
@@ -16,7 +16,7 @@ public class TestSample {
         float mulRes = calculator.mul(5, 5);
         float divRes = calculator.div(8, 2);
         
-        assertEquals(5, addRes);
+        assertEquals(8, addRes);
         assertEquals(5, subRes);
         assertEquals(25, mulRes);
         assertEquals(4, divRes);
