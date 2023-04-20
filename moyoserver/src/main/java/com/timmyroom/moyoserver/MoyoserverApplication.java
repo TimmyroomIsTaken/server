c6df999a0e15f5b1ecaf2f88dd8c7abbeac93a39
shin0343 <shin0343@mail.hongik.ac.kr>
2023-04-20 20:59:05 +0900

    SpringBoot 프로젝트 신규 생성
     - 패키지 이름 수정
     - lombok 라이브러리 추가

---
+++ moyoserver/src/main/java/com/timmyroom/moyoserver/MoyoserverApplication.java
@@ -0,0 +1,13 @@
+package com.timmyroom.moyoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyoserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoyoserverApplication.class, args);
+	}
+
+}
