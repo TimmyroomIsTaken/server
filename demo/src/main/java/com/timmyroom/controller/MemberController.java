6bb8714a39a953b1de4f9c15634db956e09948ae
shin0343 <shin0343@mail.hongik.ac.kr>
2023-04-20 20:19:46 +0900

    회원 홈화면/등록/조회 구현

--- demo/src/main/java/com/timmyroom/controller/MemberController.java
+++ demo/src/main/java/com/timmyroom/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.timmyroom.controller;
 
+import com.timmyroom.domain.Member;
 import com.timmyroom.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 스프링 컨테이너가 관리하게됨
 public class MemberController {
@@ -11,5 +17,27 @@ public class MemberController {
     @Autowired // 스프링이 Controller가 생성될때 서비스를 연결해줌 (=DI=의존성 주입)
     public MemberController(MemberService memberService) { // 스프링은 MemberService를 알 길이 없다
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/memvers/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
