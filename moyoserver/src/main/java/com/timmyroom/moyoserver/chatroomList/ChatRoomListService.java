c13d68e4b55db8bef48619858955847db094efad
shin0343 <shin0343@mail.hongik.ac.kr>
2023-05-28 10:13:55 +0900

    [Feat] mongoDB 연동 완료, 서버 실행시 DB동작부(insert) 구현

--- moyoserver/src/main/java/com/timmyroom/moyoserver/chatroomList/ChatRoomListService.java
+++ moyoserver/src/main/java/com/timmyroom/moyoserver/chatroomList/ChatRoomListService.java
@@ -1,6 +1,8 @@
 package com.timmyroom.moyoserver.chatroomList;
 
 import com.timmyroom.moyoserver.chatroomList.dto.ChatRoomInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,12 +11,13 @@ import java.util.List;
 @Service
 public class ChatRoomListService {
 
-    public List<ChatRoomInfo> getChatRoomList() {
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
-        // TO-DO : 서비스 메소드 호출하여 채팅방 리스트 가져오기 (Controller<->Service<->Repository<->DB)
-        List<ChatRoomInfo> chatRoomInfoList = new ArrayList<ChatRoomInfo>();
-        chatRoomInfoList.add(new ChatRoomInfo("채팅방1",true,4,"추감기"));
-        chatRoomInfoList.add(new ChatRoomInfo("채팅방2",true,4,"파스칼"));
+    public List<ChatRoomInfo> getChatRoomList() {
+        // 서비스 메소드 호출하여 채팅방 리스트 가져오기 (Controller<->Service<->Repository<->DB)
+
+        List<ChatRoomInfo> chatRoomInfoList = mongoTemplate.findAll(ChatRoomInfo.class, "chat_room_info");
 
         return chatRoomInfoList;
     }
--- moyoserver/src/main/java/com/timmyroom/moyoserver/chatroomList/dto/ChatRoomInfo.java
+++ moyoserver/src/main/java/com/timmyroom/moyoserver/chatroomList/dto/ChatRoomInfo.java
@@ -1,16 +1,23 @@
 package com.timmyroom.moyoserver.chatroomList.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 
 @Getter
 @NoArgsConstructor
+@Document(collection = "chat_room_info")
 public class ChatRoomInfo {
+    @Id
     private String name;
     private boolean isActivated;
     private int participant;
     private String hostName;
 
+    @Builder
     public ChatRoomInfo(String name, boolean isActivated, int participant, String hostName) {
         this.name = name;
         this.isActivated = isActivated;
